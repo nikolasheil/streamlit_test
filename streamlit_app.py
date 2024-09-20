7246d623f90a0aa060a2163c5d13f3b67f59492f
nikolasheil <n.heil@gmx.de>
2024-09-20 07:03:37 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Hello World")
 
 st.write("*Data Science Course 2024*")
+
+df = pd.read_csv("data/Bastar Craton.csv")
+
+st.dataframe(df)
