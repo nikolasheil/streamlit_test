f35ce4bd3ff5066d74310a2ec9cf95b74afe000e
nikolasheil <n.heil@gmx.de>
2024-09-20 07:24:50 +0000

    sws

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 st.title("Hello World")
 
 st.write("*Data Science Course 2024*")
 
 df = pd.read_csv("data/Bastar Craton.csv")
+
+cat_names = df.columns.tolist()[27:]
+
+el1 = st.selectbox("select element", cat_names)
+st.write(el1)
 
 st.dataframe(df)
