172bf01471b224de0e83bd560fb26f69dfc67180
nikolasheil <n.heil@gmx.de>
2024-09-20 07:45:27 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
+!pip install matplotlib
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 st.title("Hello World")
 
@@ -10,7 +11,15 @@ df = pd.read_csv("data/Bastar Craton.csv")
 
 cat_names = df.columns.tolist()[27:]
 
-el1 = st.selectbox("select element", cat_names)
-st.write(el1)
+col1, col2 =st.columns(2)
+
+with col1:
+    el1 = st.selectbox("x-axis", cat_names)
+    el2 = st.selectbox("y-axis", cat_names)
+
+with col2:
+    fig=plt.figure
+    plt.scatter([1,2,3], [4,5,6])
+    st.pyplot(fig)
 
 st.dataframe(df)
